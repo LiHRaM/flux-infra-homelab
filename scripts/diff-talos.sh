8c2cb032859a8cc8346f8845d04c50ce042f3b42
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2024-07-01 09:55:56 +0200

    feat(scripts): diff all main kustomizations

--- scripts/diff-talos.sh
+++ scripts/diff-talos.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
-flux diff kustomization flux-system --path ./clusters/talos
+
+CLUSTER=talos
+
+set -eu pipefail
+
+echo "Diffing flux-system"
+flux diff kustomization flux-system --path ./clusters/"$CLUSTER"
+
+echo "Diffing infrastructure"
+flux diff kustomization infrastructure --path ./infrastructure/"$CLUSTER"
+
+echo "Diffing apps"
+flux diff kustomization apps --path ./apps/"$CLUSTER"
